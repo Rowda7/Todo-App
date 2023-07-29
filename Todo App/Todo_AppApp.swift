e11867ca96fb7a76c8117cecd067624db67a53ba
Rowda7 <rw.sh7720@gmail.com>
2023-07-29 17:04:36 +0400

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Rowdah Alshehhi on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
